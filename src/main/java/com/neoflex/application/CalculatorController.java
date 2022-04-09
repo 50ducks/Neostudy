e8984491a8a50c729c3e0286b304a3cdaa5315ab
50ducks <50ducks@users.noreply.github.com>
2022-04-09 19:00:23 +0300

    )

--- src/main/java/com/neoflex/application/CalculatorController.java
+++ src/main/java/com/neoflex/application/CalculatorController.java
@@ -5,29 +5,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 @RestController
 public class CalculatorController {
 
     @GetMapping("/math/addition")
     public float addition (@RequestParam(value = "varA", defaultValue = "0") float varA,
-                             @RequestParam(value = "varB", defaultValue = "0") float varB) throws Exception {
+                             @RequestParam(value = "varB", defaultValue = "0") float varB) {
         return Addition.add(varA, varB);
     }
     @GetMapping("/math/subtracting")
     public float subtraction (@RequestParam(value = "varA", defaultValue = "0") float varA,
-                             @RequestParam(value = "varB", defaultValue = "0") float varB) throws Exception {
+                             @RequestParam(value = "varB", defaultValue = "0") float varB) {
         return Subtraction.sub(varA, varB);
     }
     @GetMapping("/math/multiplication")
     public float multiplication (@RequestParam(value = "varA", defaultValue = "0") float varA,
-                             @RequestParam(value = "varB", defaultValue = "0") float varB) throws Exception {
+                             @RequestParam(value = "varB", defaultValue = "0") float varB) {
         return Multiplication.multiple(varA, varB);
     }
     @GetMapping("/math/division")
     public float division (@RequestParam(value = "varA", defaultValue = "0") float varA,
-                             @RequestParam(value = "varB", defaultValue = "0") float varB) throws Exception {
+                             @RequestParam(value = "varB", defaultValue = "0") float varB) {
         return Division.div(varA, varB);
     }
 }
